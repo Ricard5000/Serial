f3d5dd18dcf1dc98d21b047ff093b3c8c013ae3c
Ricard Garcia <ricardgarciaizquierdo@gmail.com>
2016-02-12 19:28:27 +0100

    Parlar
    El que aquets programa fa és el seguent; tu li introdueixes un text que
    serà el que et dirà l'arduino.

---
+++ sketch_feb12a/sketch_feb12a.ino
@@ -0,0 +1,22 @@
+/**************************************************************
+**                                                           **
+**                   Descripció programa                     **
+**                                                           **
+**************************************************************/
+
+//******  Includes  *******************************************
+
+
+//******  Variables  ******************************************
+
+
+//******  Setup  **********************************************
+
+void setup() {
+  Serial.begin(9600);              //9600 es la velocitat amb la que es comunica l'arduino amb el pc.
+  Serial.println("Hello world !"); //Escrius el que vols que digui l'arduino cap a tu entre "". ln vol dir nínia nova.
+  }
+
+void loop() {
+ 
+}
