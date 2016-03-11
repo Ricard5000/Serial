3c16c10bf23f865c537ddaeb9b39af97f5e8ffe1
Ricard Garcia <ricardgarciaizquierdo@gmail.com>
2016-03-11 11:27:57 +0100

    Day is dark

---
+++ Projecte_4.5/Projecte_4.5.ino
@@ -0,0 +1,40 @@
+/**************************************************************
+**                                                           **
+**                   Descripció programa                     **
+**                                                           **
+**************************************************************/
+
+//******  Includes  *******************************************
+
+
+//******  Variables  ******************************************
+int sensorReading = 0;
+//******  Setup  **********************************************
+void setup() {
+Serial.begin(9600);
+Serial.print("The day is "); 
+   
+switch (sensorReading) {
+case 0:    
+Serial.println("dark");
+break;
+case 1:    
+Serial.println("dim");
+break;
+case 2:    
+Serial.println("medium");
+break;
+case 3:
+Serial.println("bright");
+break;
+default:
+Serial.println("... I don't know!!!");
+  } 
+}
+
+
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
